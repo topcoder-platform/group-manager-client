02527c9ec39e49f61b4ea3394ab7f6368378c318
Ritesh Gupta <riteshgupta@Riteshs-MacBook-Pro.local>
2020-01-17 17:12:10 +0530

    Renamed config to develop from dev

--- config/constants/index.js
+++ config/constants/index.js
@@ -1,9 +1,9 @@
 module.exports = (() => {
-  const branch = process.env.CIRCLE_BRANCH || 'dev'
+  const branch = process.env.CIRCLE_BRANCH || 'develop'
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (['master', 'qa'].indexOf(branch) < 0) {
-    return require('./dev')
+    return require('./develop')
   }
 
   return require('./' + branch)

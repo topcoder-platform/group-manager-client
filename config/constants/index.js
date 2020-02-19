a7d2261a04bb7fe4526d96b7078153a873c9414a
Ritesh Gupta <riteshgupta@Riteshs-MacBook-Pro.local>
2020-02-19 15:27:30 +0530

    Set config

--- config/constants/index.js
+++ config/constants/index.js
@@ -1,5 +1,5 @@
 module.exports = (() => {
-  const branch = process.env.CIRCLE_BRANCH || 'develop'
+  const branch = 'master' //process.env.CIRCLE_BRANCH || 'master'
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (['master', 'qa'].indexOf(branch) < 0) {
